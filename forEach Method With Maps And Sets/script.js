c1937cf6fdb793196e9e89deb776efc968ba9983
muhammadMusa666 <Unkox1890@gmail.com>
2026-07-15 23:15:59 +0500

    Made With Love By Musa!

---
+++ forEach Method With Maps And Sets/script.js
@@ -0,0 +1,105 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// Below Is Where We Will Be Learning About Working With Arrays,
+// Above Is The Code For The Project,
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+// Today We Will Be Learning About Using The forEach() Method,
+// With Maps And Sets, This Subrepo Is More Of A Detour, But Its Still Good To Know,
+// So, Lettuce Begin🥬!
+
+// I'll Be Taking The Currencies Map From Above, Commenting It Out There,
+// And Placing It Right Below,
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Now, Let's Do The Same Thing With A Set,
+// First, I'll Create A Set Of Unique Currencies,
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+
+// Then, I'll Use The forEach() Method To Log Each Value In The Set,
+currenciesUnique.forEach(function (value, _, set) { // <-- An Underscore In JavaScript Is Used For Throwaway Variables, Meaning Variables That Are Completely Unecessary,
+  console.log(`${value}: ${value}`);
+});
+// Thats All For Today, This Was One Of The Shortest Subrepo Ive Ever Done,
+// I Mean It Was A Detour So Keeping It Short Would Be Ideal,
+// But Anyways, Keep On Analyzing, Practicing, Learning, And Researching,
+// And Happy Coding😄💻🩵💙!
